83848da5a616c470718807b05f8769744a70559f
pawel-turowski <pawel1546@gmail.com>
2019-11-24 19:08:49 +0100

    repozytorium w trakcie trwania kursu

--- prosty_kalkulator.py
+++ prosty_kalkulator.py
@@ -17,21 +17,30 @@ def dzielenie(a, b):
 def potęgowanie(a, b):
     return a**b
 
-def pierwiastek(a):
-    return math.sqrt(a)
+def pierwiastek(c):
+    return math.sqrt(c)  # c = a + b
+
+def logarytm(a, b):
+    return math.log(a, b)
+
+
+
 
 if __name__ == '__main__':
     user_a = int(input('podaj liczbę: '))
     user_b = int(input('podaj liczbę: '))
+    user_c = user_a + user_b
     wynik_dodawania = dodawanie(user_a, user_b)
     wynik_odejmowania = odejmowanie(user_a, user_b)
     wynik_mnożenia = mnożenie(user_a, user_b)
     wynik_dzielenia = dzielenie(user_a, user_b)
     wynik_potęgowania = potęgowanie(user_a, user_b)
-    wynik_pierwiastkowania = pierwiastek(user_a)
+    wynik_pierwiastkowania = pierwiastek(user_c)
+    wynik_logarytmu = logarytm(user_a, user_b)
     print(f'dodawanie: {wynik_dodawania}')
     print(f'odejmowanie: {wynik_odejmowania}')
     print(f'mnożenie: {wynik_mnożenia}')
     print(f'dzielenie: {wynik_dzielenia}')
     print(f'potęgowanie: {wynik_potęgowania}')
     print(f'pierwiastek: {wynik_pierwiastkowania}')
+    print(f'logarytm: {wynik_logarytmu}')
